8887977339eb4ff4c01c20f1d4cbd5cea2cac9f7
ApasoftTraining <apasoft.training@gmail.com>
2021-04-14 09:54:06 +0200

    Iniciales

--- src/main/scala/curso/Iniciales.scala
+++ src/main/scala/curso/Iniciales.scala
@@ -2,9 +2,16 @@ package curso
 
 object Iniciales {
   def main(args: Array[String]): Unit = {
+    var nombre = scala.io.StdIn.readLine("Introduce el nombre")
+    var ape1 = scala.io.StdIn.readLine("Introduzca el apellido1")
+    var ape2 = scala.io.StdIn.readLine("Introduzca el apellido2")
 
+    println(iniciales(nombre, ape1, ape2))
   }
 
+  def iniciales(nom: String, apl1: String, apl2: String): String = {
+    nom.substring(0, 1).toUpperCase + "." + apl1.substring(0, 1).toUpperCase + "." + apl2.substring(0, 1).toUpperCase + "."
+  }
 
 
 }
